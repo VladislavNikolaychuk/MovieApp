c49ef7c44486fa69ff2ab4afd14c24e7c67a9b95
vladislav <vnikolaychuk@atncorp.com>
2022-02-11 16:56:12 +0200

    Added first commit

---
+++ MovieApp/UserStory/MovieViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MovieViewController.swift
+//  MovieApp
+//
+//  Created by Vladislav Nikolaychuk on 11.02.2022.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
